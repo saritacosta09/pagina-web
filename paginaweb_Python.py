a74031414e5e1e99af162fd9d78939b60a3fbfc3
saritacosta09 <134511014+saritacosta09@users.noreply.github.com>
2023-06-28 08:17:19 -0500

    Create paginaweb_Python.py

---
+++ paginaweb_Python.py
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+ <title>Mi primera página web</title>
+</head>
+<body>
+ <h1>Conooce mas sobre mi &#9996; </h1>
+
+ <font color="purple"><h2>Sobre mí</h2></font> 
+ <p>¡Hola! Soy Sarita Acosta, una estudiante de undecumo grado, tengo 16 años, vivo en la ciudad de San Juan de Pasto, soy apasionada por la programacion y tecnologia, me encanta progresar, anhelo ser una empresaria joven y una mujer exitosa </p>
+
+ <font color="purple"><h2>Mis pasatiempos</h2></font>
+ <ul>
+ <li>Bailar</li>
+ <li>Cantar</li>
+ <li>Salir a pasear</li>
+ <li>Hacer ejercicio</li>
+ </ul>
+
+ <font color="purple"><h2>Mis aspiraciones</h2></font>
+ <ul>
+ <li>Graduarme de grado 11</li>
+ <li>Sacar un buen puntaje en el icfes</li>
+ <li>Entrar a la universidad</li>
+ <li>Tener mi emprendimiento de ropa</li>
+ <li>Tener empresas</li>
+ <li>Ser exitosa</li>
+ <li>Ser una trader rentable</li>
+<img src= "https://us.123rf.com/450wm/rastudio/rastudio1612/rastudio161200209/66786756-cauc%C3%A1sico-exitosa-mujer-de-negocios-de-pie-en-un-gr%C3%A1fico-de-beneficios-joven-mujer-de-negocios.jpg" style="height:200px">
+ </ul>
+
+
+<font color="purple"><h2>Inteligencia artificial</h2></font>
+ <p>La inteligencia artificial (IA) es el campo de la ciencia informática dedicado a la resolución de problemas cognitivos asociados comúnmente a la inteligencia humana, como el aprendizaje, la resolución de problemas y el reconocimiento de patrones. La IA permite quIA permite que los sistemas tecnológicose los sistemas tecnológicos perciban su entorno, se relacionen con él, resuelvan problemas y actúen con un fin específico. </p>
+<img src= "https://www.eltiempo.com/files/article_main_1200/uploads/2023/03/08/6408cc610429f.jpeg"style="height:200px">
+
+<font color="purple"><h2>Github</h2></font>
+<p>Github es un portal creado para alojar el código de las aplicaciones de cualquier desarrollador, y que fue comprada por Microsoft en junio del 2018. La plataforma está creada para que los desarrolladores suban el código de sus aplicaciones y herramientas, y que como usuario no sólo puedas descargarte la aplicación, sino también entrar a su perfil para leer sobre ella o colaborar con su desarrollo.</p>
+<img src= "https://global-uploads.webflow.com/5f5a53e153805db840dae2db/6073fbf151fa4565d48572dc_GitHub_aprender-programaci%25C3%25B3n.jpeg" style="height:200px">
